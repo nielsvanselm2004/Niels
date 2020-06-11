32fde017919a4c9bb14ff4cf4ff0f94ab217ed79
Jappie313 <66782104+jappie313@users.noreply.github.com>
2020-06-11 16:06:52 +0200

    test findSResponse

--- index.php
+++ index.php
@@ -16,10 +16,10 @@
         </form>
         <?php
         //base variables
-        $jFileW = fopen("jData.ini", "w");
+        /*$jFileW = fopen("jData.ini", "w");
         $jFileA = fopen("jData.ini", "a");
         $wFileW = fopen("wData.ini", "w");
-        $wFileA = fopen("wData.ini", "a");
+        $wFileA = fopen("wData.ini", "a");*/
         //base functions
         function getCMessage(){
             if(isset($_POST["submit"])) {
