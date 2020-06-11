7c0603cc62fd9169ce9c395343301f986ece45fb
WAlbeda <59028184+walbeda@users.noreply.github.com>
2020-06-11 16:59:30 +0200

    moved php to the top in the body

--- index.php
+++ index.php
@@ -6,6 +6,33 @@
     <title>ChatBot</title>
 </head>
 <body>
+    <?php
+    //base variables
+    $jFile = "jData.ini";
+    $wFile = "wData.ini";
+    //base functions
+    function getCMessage(){
+        if(isset($_POST["submit"])) {
+            if (!empty($_POST["cMessage"])) {
+                return $_POST["cMessage"];
+            } else {
+                return "ERROR: No message was send!";
+            }
+        }
+    }
+    function sendSResponse($response){
+        echo $response;
+    }
+    function findSResponse($cQuestion){
+        print_r(parse_ini_file($GLOBALS["jFile"],true));
+        $cQuestion = "0";
+    }
+
+    echo getCMessage();
+
+
+
+    ?>
     <div id="main">
         <h1 class="title">Een ChatBot is geen ChatRobot en al helemaal geen RobotChat.</h1>
         <p>snor</p>
@@ -14,33 +41,6 @@
             <input class="input" name="cMessage" type="text" placeholder="Your Message">
             <input class="submit" type="submit" name="submit"/>
         </form>
-        <?php
-        //base variables
-        $jFile = "jData.ini";
-        $wFile = "wData.ini";
-        //base functions
-        function getCMessage(){
-            if(isset($_POST["submit"])) {
-                if (!empty($_POST["cMessage"])) {
-                    return $_POST["cMessage"];
-                } else {
-                    return "ERROR: No message was send!";
-                }
-            }
-        }
-        function sendSResponse($response){
-            echo $response;
-        }
-        function findSResponse($cQuestion){
-            print_r(parse_ini_file($GLOBALS["jFile"],true));
-            $cQuestion = "0";
-        }
-
-        echo getCMessage();
-
-
-
-        ?>
     </div>
 </body>
 </html>
