e731f37596a42eaf88fcebdc438e41f018b96924
Jappie313 <66782104+Jappie313@users.noreply.github.com>
2020-06-11 14:30:53 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
-<?php
-echo "test2";
-?>
+<!DOCTYPE html>
+<html>
+    <h1>ZULULULU</h1>
+    <p>U SUCK JOE</p>
+    <form id="chatbot" method="post">
+        <input name="cMessage" type="text" placeholder="Your Message">
+        <label for="cMessage">EWA</label><br>
+        <input type="submit" name="submit"/>
+    </form>
+    <?php
+    
+    function getCMessage(){
+        if(isset($_POST["submit"])) {
+            if (!empty($_POST["cMessage"])) {
+                return $_POST["cMessage"];
+            } else {
+                return "ERROR: No message was send!";
+            }
+        }
+    }
+    function sendSResponse($response){
+        echo $response;
+    }
+    function findSResponse(){
+    }
+
+    echo getCMessage();
+    ?>
+</html>
