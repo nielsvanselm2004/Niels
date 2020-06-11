d5bb6547f0b4ad641e4f251b0f1617323de52e4e
WAlbeda <59028184+WAlbeda@users.noreply.github.com>
2020-06-11 15:55:10 +0200

    made it dark theme and fixed title tag in css

--- index.php
+++ index.php
@@ -6,36 +6,38 @@
     <title>ChatBot</title>
 </head>
 <body>
-    <h1 class="title">Een ChatBot is geen ChatRobot en al helemaal geen RobotChat.</h1>
-    <p>snor</p>
-    <form id="chatbot" method="post">
-        <input name="cMessage" type="text" placeholder="Your Message">
-        <label for="cMessage">EWA</label><br>
-        <input type="submit" name="submit"/>
-    </form>
-    <?php
-    //base variables
-    $jFileW = fopen("jData.ini", "w");
-    $jFileA = fopen("jData.ini", "a");
-    $wFileW = fopen("wData.ini", "w");
-    $wFileA = fopen("wData.ini", "a");
-    //base functions
-    function getCMessage(){
-        if(isset($_POST["submit"])) {
-            if (!empty($_POST["cMessage"])) {
-                return $_POST["cMessage"];
-            } else {
-                return "ERROR: No message was send!";
+    <div id="main">
+        <h1 class="title">Een ChatBot is geen ChatRobot en al helemaal geen RobotChat.</h1>
+        <p>snor</p>
+        <form id="chatbot" method="post">
+            <input name="cMessage" type="text" placeholder="Your Message">
+            <label for="cMessage">EWA</label><br>
+            <input type="submit" name="submit"/>
+        </form>
+        <?php
+        //base variables
+        $jFileW = fopen("jData.ini", "w");
+        $jFileA = fopen("jData.ini", "a");
+        $wFileW = fopen("wData.ini", "w");
+        $wFileA = fopen("wData.ini", "a");
+        //base functions
+        function getCMessage(){
+            if(isset($_POST["submit"])) {
+                if (!empty($_POST["cMessage"])) {
+                    return $_POST["cMessage"];
+                } else {
+                    return "ERROR: No message was send!";
+                }
             }
         }
-    }
-    function sendSResponse($response){
-        echo $response;
-    }
-    function findSResponse(){
-    }
+        function sendSResponse($response){
+            echo $response;
+        }
+        function findSResponse(){
+        }
 
-    echo getCMessage();
-    ?>
+        echo getCMessage();
+        ?>
+    </div>
 </body>
 </html>
