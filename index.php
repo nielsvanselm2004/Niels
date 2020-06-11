f24cd5b13b0d98392ff027ece8f4420d31cd9b68
Jappie313 <66782104+Jappie313@users.noreply.github.com>
2020-06-11 15:31:39 +0200

    update  0.1

--- index.php
+++ index.php
@@ -1,18 +1,14 @@
 <!DOCTYPE html>
-<html lang="nl">
-<head>
-    <meta charset="UTF-8">
-    <title>ChatBot</title>
-</head>
-<body>
-    <h1>Een ChatBot is geen ChatRobot en al helemaal geen RobotChat...</h1>
-    <p>snor</p>
+<html>
+    <h1>ZULULULUlululululu</h1>
+    <p>U SUCK JOE</p>
     <form id="chatbot" method="post">
         <input name="cMessage" type="text" placeholder="Your Message">
         <label for="cMessage">EWA</label><br>
         <input type="submit" name="submit"/>
     </form>
     <?php
+    
     function getCMessage(){
         if(isset($_POST["submit"])) {
             if (!empty($_POST["cMessage"])) {
@@ -27,7 +23,7 @@
     }
     function findSResponse(){
     }
+    /* ewad*/
     echo getCMessage();
     ?>
-</body>
 </html>
