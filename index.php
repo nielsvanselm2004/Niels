da6c69334012c8ce521998c17e6215aa92249c23
Jappie313 <66782104+jappie313@users.noreply.github.com>
2020-06-11 16:42:34 +0200

    test findSResponse

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
             echo $response;
         }
         function findSResponse($cQuestion){
-            print_r(parse_ini_file($GLOBALS["jFile"]));
+            print_r(parse_ini_file($GLOBALS["jFile"],true));
             $cQuestion = "0";
         }
 
